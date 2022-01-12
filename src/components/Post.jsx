4304f63dffdc33d5967f9b899dde978bfd64b4be
Juliette Mahé <mahe.juliette@outlook.com>
2022-01-12 12:14:53 +0100

    postlist and post components jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
-const Post = () => {
+const Post = ({post}) => {
     return (
-        <div className='post'>
-            
+        <div className='post-card'>
+            <Link to={`/profil/${post.userid}`}><h3>{post.name}</h3></Link>
+            <img className="post-img" src={post.picture} alt={post.name} />
+            <p>{" "}{post.caption}</p>
+            <p>Publié le{" "}{post.date}</p>
         </div>
     )
 }
