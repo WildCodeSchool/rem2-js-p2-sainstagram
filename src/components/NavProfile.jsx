584fcc38164a569f0be160ebe54f07bdcbc3ed0a
Stephen B <stephen-bailly_student2021@wilder.school>
2022-01-19 11:50:29 +0100

    random navprofile function done

--- src/components/NavProfile.jsx
+++ src/components/NavProfile.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from "axios";
 import { Link } from "react-router-dom";
-import users from '../assets/users';
+/*import users from '../assets/users';*/
 import User from './User';
 import './NavProfile.css';
 
 const NavProfile = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        Axios
+            .get("https://a.nacapi.com/sainstgram.users")
+            .then((response) => {
+                setUsers(response.data)
+            })
+    }, []);
+
+    const randomProfile = [];
+    
+    function getRandomUsers() {
+        if (users.length) {
+            for (let i = 0; i <= 5; i++) {
+                const j = Math.floor(Math.random() * users.length)
+                randomProfile.push(users[j])
+            }
+        }
+    }
+    getRandomUsers();
+
     return (
         <div className="NavProfile" >
-            {users.map((user, index) => index < 6 ? <Link to={`/Profil/${user.id}`}><User {...user} key={user.id}/></Link> : '')}
+            {randomProfile.map((user) => <Link to={`/Profil/${user.id}`}><User {...user} key={user.id} /></Link>)}
         </div>
     )
 }
