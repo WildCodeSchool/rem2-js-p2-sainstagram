32faa0e8b94e66ef52f057c8d0df7e9574d00c45
Juliette Mahé <mahe.juliette@outlook.com>
2022-01-20 18:10:41 +0100

    new changes on cardprofile css

--- src/components/NavProfile.jsx
+++ src/components/NavProfile.jsx
@@ -34,6 +34,8 @@ const NavProfile = () => {
         <div className="NavProfile" >
             {randomProfile.map((user) => <Link to={`/Profil/${user.id}`}><User {...user} key={user.id} /></Link>)}
             <img onClick={ ()=>{setTrigger({})}} className='nav-refresh-button' src={refresh2} alt="New profiles" />
+        </div>
     )        
 }
 
+export default NavProfile;
