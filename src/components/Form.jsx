6746d2b338ddf27ae343dba38ed86777523dcd51
GuillaumeN <guillaume.nivesse@yahoo.fr>
2022-01-25 14:44:28 +0100

    desktop update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,36 +3,41 @@ import { useState } from 'react'
 import "./Form.css"
 
 function Form() {
-    const [image, setImage] = useState('')
-    const [loading, setLoading] = useState(false)
+  const [image, setImage] = useState('')
+  const [loading, setLoading] = useState(false)
   function validation() {
     //to do: post data to API
   }
-      return (
-      <div className="Formulaire">
-        <h1>Ajoutez votre image</h1>
-        <form onSubmit={validation}>
-          <label for="picture">url de l'image</label>
-        <input
-          type="text"
-          name="picture"
-          placeholder="Upload an image"
-        />
-          <label for="caption">Commentaire du post</label>
-        <input
-          type="text"
-          name="caption"
-          placeholder="Your comment"
-        />
-        <input
-          type="submit">
-
+  return (
+    <div className="Formulaire">
+      <h1>Ajoutez votre image</h1>
+      <form onSubmit={validation}>
+        <ul>
+          <li>
+            <label for="picture">url de l'image :</label>
+            <input
+              type="text"
+              name="picture"
+              placeholder="Upload an image"
+            />
+          </li>
+          <li>
+            <label for="caption">Commentaire du post :</label>
+            <input
+              type="text"
+              name="caption"
+              placeholder="Your comment"
+            />
+          </li>
+          <input
+            type="submit">
           </input>
-        </form>
-      </div>
-    )
-  }
- 
+        </ul>
+      </form>
+    </div>
+  )
+}
+
 
 export default Form;
 
