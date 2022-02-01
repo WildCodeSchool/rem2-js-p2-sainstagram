cd30cf63f97c7dafa5b7a92f71d09fbce418416f
mentat51 <sylvain.fauveau@gmail.com>
2022-02-01 15:20:49 +0100

    fix

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,32 +5,18 @@ import { useNavigate } from 'react-router-dom';
 import "./Form.css"
 
 function Form() {
-  const [image, setImage] = useState('')
-  const [loading, setLoading] = useState(false)
-
   const navigate = useNavigate();
 
   function Validation(event) {
-    console.log('dans Validation');
-
-    console.log('dans Validation2');
     let url = 'https://a.nacapi.com/sainstgram.posts'
     let today = new Date().toISOString().split('T')[0].split('-').join(''); // YYYYMMDD format
     const newPost = {
       "userid": 6, "date": today, "picture": event.target.picture.value,
       "caption": event.target.caption.value
     }
-    console.log(newPost);
-    console.log('dans Validation3');
 
     fetch(url, { method: 'post', body: JSON.stringify(newPost) })
-    // fetch(url, { method: 'post', body: newPost })
-      //.then(function (response) { return response.json(); })
       .then(function (response) { return ''; })
-    //.then(function (data) { console.log("data", data) })
-    console.log('dans Validation4');
-
-    console.log('dans Validation5');
     navigate('/');
     event.preventDefault();
   }
