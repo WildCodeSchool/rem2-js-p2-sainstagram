b63357344bd6004bfb190f4be3f40c808c851f24
Juliette Mahé <mahe.juliette@outlook.com>
2022-03-03 08:29:58 +0100

    media queries ok cardprofile

--- src/assets/dataNewAPI.js
+++ src/assets/dataNewAPI.js
@@ -1,4 +1,7 @@
-/* https://api.jsonbin.io/b/621a0012c4790b3406241a82/3   posts
-modified 03/03/2022
+posts="https://api.jsonbin.io/b/621a0012c4790b3406241a82/5"  
 
-/* https://api.jsonbin.io/b/6218fbd625fb1b26b188e317/2  users */
+users="https://api.jsonbin.io/b/6218fbd625fb1b26b188e317/2" 
+
+export default posts;
+
+export default users;
